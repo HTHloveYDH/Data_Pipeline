84b549c4b67f93a4429e0c46ed2adc561c2f4e99
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-07-23 09:47:37 +0800

    Update ImageDataLoader.py

--- ImageDataLoader.py
+++ ImageDataLoader.py
@@ -7,19 +7,19 @@ class ImageDataLoader:
     def __init__(self, filename):
         self.filename = filename
 
-    def load_file(self):
+    def load_data(self):
         raise NotImplementedError(" Can not call this member function via base class 'ImageFileLoader'! ")
 
 class NormalImageDataLoader(ImageDataLoader):
     def __init__(self, filename):
         super(NormalImageDataLoader, self).__init__(filename)
     
-    def load_file(self):
+    def load_data(self):
         return Image.open(self.filename)  # self.filename can be either string or bytes stream
 
 class NpyImageDataLoader(ImageDataLoader):
     def __init__(self, filename):
         super(NpyImageDataLoader, self).__init__(filename)
     
-    def load_file(self):
+    def load_data(self):
         return np.load(self.filename).astype(np.uint8)
