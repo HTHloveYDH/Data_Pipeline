91159ba7aef60f7000cbf7aa67a76b1d088fcc94
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-07-23 16:49:27 +0800

    Update ImageDataLoader.py

--- ImageDataLoader.py
+++ ImageDataLoader.py
@@ -5,36 +5,37 @@ import numpy as np
 
 class ImageDataLoader:
     '''base class'''
-    def __init__(self, data):
+    def __init__(self, data, img_mode:str):
         self.data = data
+        self.img_mode = img_mode
 
     def load_data(self):
         raise NotImplementedError(" Can not call this member function via base class 'ImageFileLoader'! ")
 
 class NormalImageDataLoader(ImageDataLoader):
-    def __init__(self, data):
-        super(NormalImageDataLoader, self).__init__(data)
+    def __init__(self, data, img_mode:str):
+        super(NormalImageDataLoader, self).__init__(data, img_mode)
     
     def load_data(self):
-        return Image.open(self.data)  # self.data can be either string or bytes stream
+        return Image.open(self.data, mode=self.img_mode)  # self.data can be either string or bytes stream
 
 class ArrayImageDataLoader(ImageDataLoader):
-    def __init__(self, data):
-        super(ArrayImageDataLoader, self).__init__(data)
+    def __init__(self, data, img_mode:str):
+        super(ArrayImageDataLoader, self).__init__(data, img_mode)
     
     def load_data(self):
-        return Image.fromarray(self.data, mode='RGB')
+        return Image.fromarray(self.data, mode=self.img_mode)
 
 class NpyImageDataLoader(ImageDataLoader):
-    def __init__(self, data):
-        super(NpyImageDataLoader, self).__init__(data)
+    def __init__(self, data, img_mode:str):
+        super(NpyImageDataLoader, self).__init__(data, img_mode)
     
     def load_data(self):
         return np.load(self.data).astype(np.uint8)
 
 class NpyImageDataLoaderV2(ImageDataLoader):
-    def __init__(self, data):
-        super(NpyImageDataLoaderV2, self).__init__(data)
+    def __init__(self, data, img_mode:str):
+        super(NpyImageDataLoaderV2, self).__init__(data, img_mode)
     
     def load_data(self):
         return np.frombuffer(self.data, dtype=np.uint8)
