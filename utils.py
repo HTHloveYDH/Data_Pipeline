59aa88ec7352dc18b86a10bac231d2a99ce96751
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-07-23 14:34:21 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import os
+import json
+
+
 def filename2loc(filename:str):
     if 's3://' in filename:
         return 's3'
@@ -5,3 +9,14 @@ def filename2loc(filename:str):
         return 'gcs'
     else:
         return 'local_disk'
+
+def load_json(filename:str):
+    with open(os.path.join('.', f'{filename}.json'), 'r') as f:
+        content = json.load(f)
+    return content
+
+def load_configs():
+    dataset_config = load_json(os.path.join('.', 'dataset_config.json'))
+    training_config = load_json(os.path.join('.', 'training_config.json'))
+    dist_config = load_json(os.path.join('.', 'dist_config.json'))
+    return dataset_config, training_config
