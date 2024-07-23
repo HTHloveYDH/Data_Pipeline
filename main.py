935addb4995cee7c639c294a48b725a88c30397e
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-07-23 17:02:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ def main():
     custom_load_config = {}
     custom_model_config = {}
     custom_train_config = {}
-    # set global variables
     runtime_global_vars_map = {}
-    runtime_global_vars_map.update({'s3_bucket_name': args.s3_bucket_name})
+    # set global variables
+    runtime_global_vars_map.update(
+        {'s3_bucket_name': args.s3_bucket_name, 'img_mode': train_config['img_mode']}
+    )
     set_global_vars(**runtime_global_vars_map)
 
     ''' ________________________________________ load dataset ________________________________________ '''
@@ -34,14 +36,13 @@ def main():
     # load configuration
     input_size = (training_config['input_width'], training_config['input_height'])
     default_img_size = (training_config['default_img_width'], training_config['default_img_height'])
-    img_mode = train_config['img_mode']  # 'RGB' or 'L'
     # get image transformation for data loading
     train_transform = get_custom_train_transform(input_size, training_config['transform_version'])
     valid_transform = get_custom_valid_transform(input_size, training_config['transform_version'])
     test_transform = get_custom_test_transform(input_size, training_config['transform_version'])
     custom_load_config.update({'random_aug_config': config['random_aug_config']})
     trainset = dataset_factory.create(training_config['dataset_type']).create_dataset(
-        dataset_config['train'], input_size, default_img_size, img_mode, is_preshuffle, 
+        dataset_config['train'], input_size, default_img_size, is_preshuffle, train_transform
         **custom_load_config
     )
     trainset_loader = DataLoader(
@@ -49,14 +50,14 @@ def main():
         drop_last=True, prefetch_factor=args.prefetch_factor
     )
     validset = dataset_factory.create(training_config['dataset_type']).create_dataset(
-        dataset_config['valid'], input_size, default_img_size, img_mode, False
+        dataset_config['valid'], input_size, default_img_size, False, valid_transform
     )
     validset_loader = DataLoader(
         validset, args.global_batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=False, 
         drop_last=True, prefetch_factor=args.prefetch_factor
     )
     testset = dataset_factory.create(training_config['dataset_type']).create_dataset(
-        dataset_config['test'], input_size, default_img_size, img_mode, False
+        dataset_config['test'], input_size, default_img_size, False, test_transform
     )
     testset_loader = DataLoader(
         testset, args.global_batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=False, 
